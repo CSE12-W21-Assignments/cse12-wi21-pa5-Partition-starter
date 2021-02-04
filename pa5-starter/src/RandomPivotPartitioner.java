8e5ffed33cd3f488bc597adff5ae2e3acaac458b
Garo H Adjoian <gadjoian@ucsd.edu>
2021-02-04 08:04:37 -0800

    Good luck on PA5!

---
+++ pa5-starter/src/RandomPivotPartitioner.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+// You can (and should) add "implements Partitioner" below once you have the implementation ready
+public class RandomPivotPartitioner {
+    // DO NOT REMOVE, NEEDED FOR TESTING
+    // Use this random object in your partition method
+    Random rand = new Random();
+
+}
